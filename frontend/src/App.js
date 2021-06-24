3928a0cb726d4fd767f96c19012a4d2e3fbddb62
johnbelmdev <johnbel458@gmail.com>
2021-06-23 21:55:34 -0400

    add login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,15 @@
+
 import React from 'react'
-import { useForm } from 'react-hook-form'
+import { BrowserRouter as  Route } from 'react-router-dom';
+import Login from "./Components/Login/Login.js";
 
  
 function App() {
-  const { register, handleSubmit, errors } = useForm();
-  const onSubmit = (data) =>{
-    console.log('hello',data);
-  }
-  return (
-
-    <div>
-       <form onSubmit= {handleSubmit(onSubmit)}>
-         
-       <input type = "text" name='email'{...register('email', { required: true })}placeholder='Email' />
-       <input  type= 'password' name='password' {...register('password', { required: true })} placeholder='Password'/>
-       {errors.password && <p> Password is invalid </p>}
-
-        <input type = "submit"/>
-       </form>
-       </div>
-   
-  );
+  return(
+  <Route exact path="/login">
+  <Login />
+</Route>
+  )
 }
 
 export default App;
---
+++ frontend/src/Components/Campaignform/Campaignform.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import { useForm } from 'react-hook-form'
+
+ 
+function CampaignForm() {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (data) =>{
+    console.log('hello',data);
+  }
+  return (
+
+    <div>
+       <form onSubmit= {handleSubmit(onSubmit)}>
+         
+       <input type = "text" name='email'{...register('email', { required: true })}placeholder='Email' />
+       <input  type= 'password' name='password' {...register('password', { required: true })} placeholder='Password'/>
+       {errors.password && <p> Password is invalid </p>}
+
+        <input type = "submit"/>
+       </form>
+       </div>
+   
+  );
+}
+
+export default CampaignForm;
